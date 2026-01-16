f78eb1ae4a5b638e4efa85fc0ac0dbc75ac9097b
Darunhein <darayava2006@gmail.com>
2026-01-16 12:51:32 -0600

    I asked the user for the prices of each product and calculated the final Price with the discount

--- prices10products.java
+++ prices10products.java
@@ -5,9 +5,29 @@ import java.util Scanner;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        final float = descaunt;
-        
+        final float DISCOUNT = .10;
+        float totalPrice;
+        float finalPrice;
 
+        System.out.println("Enter the price of the first Product ");
+        float priceProduct01 = scanner.nextFloat();
+
+        System.out.println("Enter the price of the Second Product ");
+        float priceProduct02 = scanner.nextFloat();
+
+        System.out.println("Enter the price of the Third Product ");
+        float priceProduct03 = scanner.nextFloat();
+
+        System.out.println("Enter the price of the Forth Product ");
+        float priceProduct04 = scanner.nextFloat();
+
+        System.out.println("Enter the price of the Fifth Product ");
+        float priceProduct05 = scanner.nextFloat();
+
+        totalPrice = priceProduct01 + priceProduct02 + priceProduct01 + priceProduct02 + priceProduct01
+        finalPrice = totalPrice - (totalPrice * .10)
+
+        
 
 
         }
