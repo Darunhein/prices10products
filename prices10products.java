b0287235bd4948689b96e5dd39948e9cae0bdf4d
Darunhein <darayava2006@gmail.com>
2026-01-16 12:43:36 -0600

    The virables where declared

--- prices10products.java
+++ prices10products.java
@@ -3,7 +3,12 @@ import java.util Scanner;
     public class prices10products {
         
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        final float = descaunt;
         
+
+
 
         }
     }
